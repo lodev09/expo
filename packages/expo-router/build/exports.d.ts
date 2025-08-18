6027134138682c0bde4dd71a626795f19e36df73
lodev09 <lodev09@gmail.com>
2025-08-19 04:17:19 +0800

    chore: build

--- packages/expo-router/build/exports.d.ts
+++ packages/expo-router/build/exports.d.ts
@@ -13,6 +13,8 @@ export { useSitemap, SitemapType } from './views/useSitemap';
 export { ErrorBoundaryProps } from './views/Try';
 export { ErrorBoundary } from './views/ErrorBoundary';
 export type { ScreenProps } from './useScreens';
+export { Modal, type ModalProps } from './modal/Modal';
+export { ModalContextProvider } from './modal/ModalContext';
 /**
  * @hidden
  */
